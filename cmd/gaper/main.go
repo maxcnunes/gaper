56a75324ca584036f80fadbe029e62dcb65abe96
Max Claus Nunes <maxcnunes@gmail.com>
2018-11-18 13:40:51 -0200

    Prepare release 1.0.3

--- cmd/gaper/main.go
+++ cmd/gaper/main.go
@@ -12,7 +12,7 @@ var (
 	// keep the version hardcoded because on installing it through "go get/install"
 	// it doesn't apply the build tags to override it. So, it is make easier for
 	// people using in that case to find out which version they are using
-	version = "1.0.2-dev"
+	version = "1.0.3-dev"
 )
 
 func main() {
